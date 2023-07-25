660790cbdadf34523d63eabfa1e54119d635730b
katesementsova <129986681+katesementsova@users.noreply.github.com>
2023-07-25 17:38:46 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,85 @@
+//Task 1
+
+// let user = {
+//     name: "John",
+//     age: 30
+//     };
+
+// for(let key in user) {
+//     console.log(user[key])
+//     }
+
+// console.log('age' in user);
+
+// Task 2, Task 3, Task 4
+
+// let product = {
+//     name: 'productname',
+//     price: 'productprice',
+// }
+
+// product.quantity = 'productquantity';
+// product.quality = 'productquality';
+
+// for(let key in product) {
+//     console.log(product[key])
+//     }
+
+// delete product.quality;
+
+// Task 5
+
+// let menu = {
+//     width: 200, 
+//     height: 300,
+//     title: "my menu"
+// }
+
+// for (let key in menu) {
+//     if (typeof menu[key] === 'number') {
+//         menu[key] *= 2;
+//     }
+// }
+
+// console.log(menu);
+
+
+
+// Task 6
+
+// function check(a, b) {
+//     if (a>b) {
+//         return 1 
+//     }
+//     if (a<b) {
+//         return -1
+//     } 
+//     if (a==b) {
+//         return 0}
+// }
+
+// console.log(check(5, 3));
+// console.log(check(2, 8));
+// console.log(check(7, 7));
+
+// Task 7
+
+// function isEvenNumber (c, d) {
+//     if (c%2 == 0 && d%2 == 0) {
+//         return c*d
+//     }
+//     if (c%2 != 0 && d %2 != 0) {
+//         return c+d
+//     }
+//     if (c%2 != 0 && d%2 == 0) {
+//         return c
+//     }
+//     if (c%2 == 0 && d%2 != 0) {
+//         return d 
+//     }
+// }
+
+// console.log(isEvenNumber(4, 6));
+// console.log(isEvenNumber(3, 5));
+// console.log(isEvenNumber(3, 6));
+// console.log(isEvenNumber(4, 9));
